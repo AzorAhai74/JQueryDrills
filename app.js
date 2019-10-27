0534b691d9b3016d551253fd2ff07ed09eb2a59e
byron baldwin <byronsbaldwin@gmail.com>
2019-10-27 10:45:15 -0500

    No.11 and 12

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
     let submitBtn = $('#btnSubmit');
     let $myH2 = $("<h2></h2>");
+    let $myList = $("<li></li>");
     $(submitBtn).click(function() {
         function alertValue() {
             alert($('input').val());//inputs the text in the form field when the submit button is clicked//  
         }  
         $('input').change(alertValue);
         $('div').append($myH2);//creates a header when the submit button is clicked//
-        $myH2.append($(':input[type="text"]').val());//inouts the text from the form field when the submit button is clicked//
+        $myH2.append($(':input[type="text"]').val());//inputs the text from the form field when the submit button is clicked//
         event.preventDefault();
         //below when the mouse hovers over the text, the background color changes to red and the border radius increases to 10px//
         $('h2').on('mouseover', function() {
@@ -17,6 +18,13 @@ $(document).ready(function() {
             });
         })
         alertValue();
+        $('ul').append($myList);
+        $myList.append($(':input[type="text"]').val());
+        $('li').on('click', function() {
+            $(this).css({
+                
+            })
+        })
         });
     $(':input[type="submit"]').prop('disabled', true);
         $('input[type="text"]').keyup(function() {
@@ -25,6 +33,7 @@ $(document).ready(function() {
             }
         });
     $('form').after('<div></div>');
+    $('body').append("<ul id='newList'></ul");
 });
 
 
